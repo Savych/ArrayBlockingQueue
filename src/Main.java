cf7003a51e42fca3d6663076eaf5c8ea6093d89f
Savych <savasawa55@gmail.com>
2018-04-24 23:33:25 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class Main {
+    private static ArrayBlockingQueue<Object> list = new ArrayBlockingQueue<>(1000);
+    public static void main(String[] args) {
+        addObjects();
+        proceed();
+    }
+
+    private static void proceed() {
+        System.out.println(list.size());
+        for(Object element : list) {
+           list.remove(element);
+        }
+        System.out.println(list.size());
+    }
+
+    private static void addObjects() {
+        for(int i = 0; i < 5; i++) {
+            list.add(new Object());
+        }
+    }
+}
